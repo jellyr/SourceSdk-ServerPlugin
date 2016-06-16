85c2103bd447cb27c6b0020b60e80ed249a62375
L-EARN <le.padellec.sylvain@gmail.com>
2016-06-16 19:43:39 +0200

    Restore IPlayerInfo
    because it actually is the same for CS:S and CS:GO

--- Interfaces/IPlayerInfoManager/IPlayerInfoManager002.h
+++ Interfaces/IPlayerInfoManager/IPlayerInfoManager002.h
@@ -1,12 +1,14 @@
 #ifndef IPLAYERINFOMANAGER002_H
 #define IPLAYERINFOMANAGER002_H
 
+class IPlayerInfo;
+
 namespace SourceSdk
 {
 	abstract_class IPlayerInfoManager002
 	{
 	public:
-		virtual void *GetPlayerInfo( edict_t *pEdict ) = 0;
+		virtual IPlayerInfo * GetPlayerInfo( edict_t *pEdict ) = 0;
 		virtual void *GetGlobalVars() = 0;
 	};
 };
